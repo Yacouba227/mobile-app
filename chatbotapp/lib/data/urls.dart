c1f2553c6952bbb9a2ba7933a4c4ab5a51a63e58
Yacouba227 <halidouyacouba999@gmail.com>
2026-01-21 17:26:30 +0100

    api key...

--- chatbotapp/lib/data/urls.dart
+++ chatbotapp/lib/data/urls.dart
@@ -1,4 +1,4 @@
 class Urls {
   static const String geminiBaseUrl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent";
-  static const String apiKey = "AIzaSyBQao4FN9R8-w1WT6fx4fW0pfRCc5bntGk";
+  static const String apiKey = "";
 }
